ce6f02229113625c73b67f78f6b4dcf3d4e57da1
Ethan <ethansagin@gmail.com>
2019-02-28 18:24:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,11 +7,10 @@ def line(deli)
   else
     current_string += (":")
     counter = 0
-    until counter == deli.length - 1
-      current_string += " #{counter + 1}. #{deli[counter]},"
+    until counter == deli.length
+      current_string += " #{counter + 1}. #{deli[counter]}"
       counter += 1
     end
-    current_string += " #{deli.length}. #{deli[deli.length - 1]}"
     puts current_string
   end
 end
