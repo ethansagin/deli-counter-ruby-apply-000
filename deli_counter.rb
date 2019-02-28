5a15e7234797082f715d9f3d46eb6a8fa13ae517
Ethan <ethansagin@gmail.com>
2019-02-28 18:13:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,3 @@
-katz_deli = []
-
-def line(deli)
-  current_string = "The line is currently"
-  if deli.length == 0
-    current_string.push(" empty.")
-  else
-    current_string.push(":")
-    counter = 0
-    until counter == deli.length
-      current_string.push(" #{counter + 1}. #{deli[counter]},")
-      counter += 1
-end
 
 def take_a_number(array, newbie)
   array.push(newbie)
