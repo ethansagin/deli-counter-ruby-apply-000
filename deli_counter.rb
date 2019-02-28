1caff2a47294dbf8e6fef3b449fa04ca01001404
Ethan <ethansagin@gmail.com>
2019-02-28 18:19:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,12 @@ def line(deli)
   current_string = "The line is currently"
   if deli.length == 0
     puts current_string + " empty."
+      else
+    current_string += (":")
+    counter = 0
+    until counter == deli.length
+      current_string += " #{counter + 1}. #{deli[counter]},")
+      counter += 1
+    end
   end
 end
