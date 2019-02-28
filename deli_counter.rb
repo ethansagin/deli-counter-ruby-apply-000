6191bd138b5bf7087e75286a3ec83310f8374ce2
Ethan <ethansagin@gmail.com>
2019-02-28 18:15:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,13 +13,3 @@ def line(deli)
     end
   end
 end
-
-
-def take_a_number(array, newbie)
-  array.push(newbie)
-  puts "#{newbie}, you are number #{array.length} in line."
-end
-
-def now_serving
-  if 
-end
